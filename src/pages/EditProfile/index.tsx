d898a518e1036c8b4593030d2dd3e8ad323d1243
kadekadhi120 <i.nugraha005@binus.ac.id>
2024-12-11 18:04:54 +0700

    Fix page profile & edit profile

--- src/pages/EditProfile/index.tsx
+++ src/pages/EditProfile/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonButton, IonItem, IonLabel, IonInput } from "@ionic/react";
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonButton, IonButtons, IonItem, IonLabel, IonInput, IonCard } from "@ionic/react";
 import { mailOutline, callOutline, locationOutline } from "ionicons/icons";
-import { ArrowBigLeft, ChevronLeft } from "lucide-react";
+import { ArrowBigLeft, ChevronLeft, Radius } from "lucide-react";
 import "./editprofile.css";
 
 export default function EditProfile() {
@@ -9,40 +9,49 @@ export default function EditProfile() {
     <IonPage>
       <IonHeader>
         <IonToolbar color="primary">
-          <IonButton routerLink="/tabs/profil">
-            <ChevronLeft />
-          </IonButton>
+          <IonButtons slot="start">
+            <IonButton routerLink="/tabs/profil">
+              <ChevronLeft />
+            </IonButton>
+          </IonButtons>
           <IonTitle>Edit Profile</IonTitle>
         </IonToolbar>
       </IonHeader>
+
       <IonContent>
         <div className="edit-profile-container">
           <div className="profile-header">
             <div className="profile-image"></div>
-            <h2 className="profile-name">Edit Profile</h2>
+            <h2 className="profile-name">Name</h2>
           </div>
 
-          {/* Form untuk mengedit data */}
-          <form onSubmit={(e) => e.preventDefault()}>
-            <IonItem>
-              <IonLabel position="stacked">Email</IonLabel>
-              <IonInput type="email" placeholder="Enter your email" />
-            </IonItem>
+          <IonCard className="editprofile-card">
+            {/* Form untuk mengedit data */}
+            <form onSubmit={(e) => e.preventDefault()}>
+              <IonItem>
+                <IonLabel position="stacked">Email</IonLabel>
+                <IonInput type="email" placeholder="Enter your email" />
+              </IonItem>
 
-            <IonItem>
-              <IonLabel position="stacked">Phone</IonLabel>
-              <IonInput type="tel" placeholder="Enter your phone number" />
-            </IonItem>
+              <IonItem>
+                <IonLabel position="stacked">Phone</IonLabel>
+                <IonInput type="tel" placeholder="Enter your phone number" />
+              </IonItem>
 
-            <IonItem>
-              <IonLabel position="stacked">Address</IonLabel>
-              <IonInput type="text" placeholder="Enter your address" />
-            </IonItem>
+              <IonItem>
+                <IonLabel position="stacked">Address</IonLabel>
+                <IonInput type="text" placeholder="Enter your address" />
+              </IonItem>
 
-            <IonButton expand="block" color="primary" style={{ marginTop: "20px" }}>
-              Save Changes
-            </IonButton>
-          </form>
+              <IonButton expand="block" className="save-button" style={{ marginTop: "20px" }}>
+                Save Changes
+              </IonButton>
+
+              {/* <IonButton expand="block" color="primary" className="rounded-button" style={{ marginTop: "20px" }}>
+                Save Changes
+              </IonButton> */}
+            </form>
+          </IonCard>
         </div>
       </IonContent>
     </IonPage>
